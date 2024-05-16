c4b5e64ea13af85fcc401ddf3896779288b1fa90
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-16 23:00:51 +0530

    Update py10.py

--- py10.py
+++ py10.py
@@ -1,9 +1,9 @@
 #nested condition in python!!
-#row = int(input("Enter number of rows: "))
-#column = int(input("Enter number of columns: "))
-#symbol = input("Enter a symbol: ")
+row = int(input("Enter number of rows: "))
+column = int(input("Enter number of columns: "))
+symbol = input("Enter a symbol: ")
 
-#for i in range(row):
-#    for j in range(column):
-#        print(symbol,end="")
-#    print() 
+for i in range(row):
+    for j in range(column):
+        print(symbol,end="")
+    print() 
